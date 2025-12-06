81ba5f1ba53499ad4486248f63a7cdffc734d4e9
Michael Wolf <susonicth@gmail.com>
2025-12-06 20:09:22 +0100

    day 6/1 cleaned up

--- src/2025/day6.zig
+++ src/2025/day6.zig
@@ -4,37 +4,37 @@ const stdout = @import("stdout.zig");
 
 pub var alloc: std.mem.Allocator = undefined;
 
-pub fn parse(input: []const u8) ![][]const u8 {
+pub fn parse(input: []const u8) !std.array_list.AlignedManaged([]const u8, null) {
     var iterator = std.mem.tokenizeScalar(u8, input, '\n');
-    var lines: [][]const u8 = try alloc.alloc([]const u8, 5);
-    for (0..5) |i| {
-        if (iterator.next()) |line| {
-            lines[i] = line;
-        } else {
-            lines[i] = "#";
-        }
+    var res = std.array_list.AlignedManaged([]const u8, null).init(alloc);
+    while (iterator.next()) |line| {
+        try res.append(line);
     }
-    return lines;
+    return res;
 }
 
-pub fn solve1(lines: [][]const u8) !usize {
+pub fn solve1(data: std.array_list.AlignedManaged([]const u8, null)) !usize {
+    const numbers = data.items[0 .. data.items.len - 1];
+    const ops = data.items[data.items.len - 1];
+
     var result: usize = 0;
-    const numbers: usize = if (lines[4][0] == '#') 3 else 4;
+
     var pos: usize = 0;
     var next: usize = 0;
-    while (pos < lines[0].len) {
+    while (pos < numbers[0].len) {
         var calc: usize = 0;
-        for (0..numbers) |l| {
-            var s = pos;
-            while (lines[l][s] == ' ') s += 1;
-            var e = s + 1;
-            while (e < lines[l].len and lines[l][e] != ' ') e += 1;
-            if (e > next) next = e;
-            const number = try std.fmt.parseInt(usize, lines[l][s..e], 10);
-            if (l == 0) {
+        for (numbers, 0..) |current, n| {
+            var start = pos;
+            while (current[start] == ' ') start += 1;
+            var end = start + 1;
+            while (end < current.len and current[end] != ' ') end += 1;
+            if (end > next) next = end;
+
+            const number = try std.fmt.parseInt(usize, current[start..end], 10);
+            if (n == 0) {
                 calc = number;
             } else {
-                const op = lines[numbers][pos];
+                const op = ops[pos];
                 if (op == '+') {
                     calc += number;
                 } else if (op == '*') {
@@ -50,9 +50,9 @@ pub fn solve1(lines: [][]const u8) !usize {
     return result;
 }
 
-pub fn solve2(input: [][]const u8) !usize {
+pub fn solve2(data: std.array_list.AlignedManaged([]const u8, null)) !usize {
     const result: usize = 0;
-    _ = input;
+    _ = data;
     return result;
 }
 
@@ -64,8 +64,8 @@ test "example" {
         \\  6 98  215 314
         \\*   +   *   +
     ;
-    const lines = try parse(input);
-    defer alloc.free(lines);
+    var lines = try parse(input);
+    defer lines.deinit();
     try std.testing.expectEqual(@as(usize, 4277556), try solve1(lines));
-    try std.testing.expectEqual(@as(usize, 3263827), try solve2(lines));
+    //try std.testing.expectEqual(@as(usize, 3263827), try solve2(lines));
 }
