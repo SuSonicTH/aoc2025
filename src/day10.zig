f6e9999315ab22169dbb8d9f5faf2386e1fb3e89
Michael Wolf <susonicth@gmail.com>
2025-12-12 00:55:00 +0100

    day 11

--- src/day10.zig
+++ src/day10.zig
@@ -1,10 +1,68 @@
 const std = @import("std");
 pub const stdout = @import("stdout.zig");
 
+const Lights = struct {
+    bulbs: u10,
+    mask: u10,
+};
+
+const Machine = struct {
+    lights: Lights,
+    buttons: []u10,
+};
+
 pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    _ = input;
-    _ = allocator;
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    var machines = std.array_list.AlignedManaged(Machine, null).init(alloc);
+    defer machines.deinit();
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        const endOfLights = std.mem.indexOfScalar(u8, line, ']').?;
+        var buttons = std.array_list.AlignedManaged(u10, null).init(alloc);
+
+        var start = std.mem.indexOfScalar(u8, line[endOfLights..], '(');
+        while (start != null) {
+            const end = std.mem.indexOfScalar(u8, line[start.?..], ')').?;
+            try buttons.append(getButton(line[start.? + 1 .. end]));
+            start = std.mem.indexOfScalar(u8, line[end..], '(');
+        }
+        try machines.append(.{
+            .lights = getLights(line[1..endOfLights]),
+            .buttons = try buttons.toOwnedSlice(),
+        });
+    }
     return 0;
+}
+
+fn getLights(lights: []const u8) Lights {
+    var value: u10 = 0;
+    var mask: u10 = 0;
+    var pos: u4 = 0;
+    for (lights) |light| {
+        if (light == '#') {
+            value |= (@as(u10, 1) << pos);
+        }
+        mask |= (@as(u10, 1) << pos);
+        pos += 1;
+    }
+    return .{
+        .bulbs = value,
+        .mask = mask,
+    };
+}
+
+fn getButton(button: []const u8) u10 {
+    var value: u10 = 0;
+    var digits = std.mem.tokenizeScalar(u8, button, ',');
+    while (digits.next()) |digit| {
+        const v: u4 = @intCast(digit[0] - '0');
+        value |= (@as(u10, 1) << v);
+    }
+    return value;
 }
 
 pub fn part2(input: []const u8, allocator: std.mem.Allocator) !usize {
@@ -23,4 +81,27 @@ test "example" {
     ;
     try std.testing.expectEqual(@as(usize, 0), try part1(input, std.testing.allocator));
     try std.testing.expectEqual(@as(usize, 0), try part2(input, std.testing.allocator));
+}
+
+test "getLights" {
+    try std.testing.expectEqual(Lights{ .lights = 0, .mask = 1023 }, getLights(".........."));
+    try std.testing.expectEqual(Lights{ .lights = 1, .mask = 1023 }, getLights("#........."));
+    try std.testing.expectEqual(Lights{ .lights = 2, .mask = 1023 }, getLights(".#........"));
+    try std.testing.expectEqual(Lights{ .lights = 3, .mask = 511 }, getLights("##......."));
+    try std.testing.expectEqual(Lights{ .lights = 4, .mask = 255 }, getLights("..#....."));
+    try std.testing.expectEqual(Lights{ .lights = 5, .mask = 127 }, getLights("#.#...."));
+    try std.testing.expectEqual(Lights{ .lights = 6, .mask = 63 }, getLights(".##..."));
+    try std.testing.expectEqual(Lights{ .lights = 512, .mask = 1023 }, getLights(".........#"));
+}
+
+test "getButton" {
+    try std.testing.expectEqual(@as(u10, 1), getButton("0"));
+    try std.testing.expectEqual(@as(u10, 2), getButton("1"));
+    try std.testing.expectEqual(@as(u10, 3), getButton("0,1"));
+    try std.testing.expectEqual(@as(u10, 5), getButton("0,2"));
+    try std.testing.expectEqual(@as(u10, 6), getButton("1,2"));
+    try std.testing.expectEqual(@as(u10, 7), getButton("0,1,2"));
+    try std.testing.expectEqual(@as(u10, 8), getButton("3"));
+    try std.testing.expectEqual(@as(u10, 512), getButton("9"));
+    try std.testing.expectEqual(@as(u10, 513), getButton("0,9"));
 }
--- src/day11.zig
+++ src/day11.zig
@@ -1,26 +1,132 @@
 const std = @import("std");
 pub const stdout = @import("stdout.zig");
 
+const maxOutputs = 23;
+const maxDevices = 26 * 26 * 26;
+
+const out = nameToId("out");
+const you = nameToId("you");
+const svr = nameToId("svr");
+const dac = nameToId("dac");
+const fft = nameToId("fft");
+
+fn parse(input: []const u8, allocator: std.mem.Allocator) ![][maxOutputs]u16 {
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    var devices = try allocator.alloc([maxOutputs]u16, maxDevices + 1);
+
+    while (lines.next()) |line| {
+        const d = nameToId(line);
+        var p: usize = 4;
+        var i: usize = 0;
+        while (p < line.len) {
+            const id = nameToId(line[p + 1 ..]);
+            devices[d][i] = id;
+            p += 4;
+            i += 1;
+        }
+        devices[d][i] = 0;
+    }
+    return devices;
+}
+
+fn nameToId(name: []const u8) u16 {
+    return @as(u16, name[0] - 'a') * 26 * 26 + @as(u16, name[1] - 'a') * 26 + @as(u16, name[2] - 'a') + 1;
+}
+
 pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    _ = input;
-    _ = allocator;
-    return 0;
+    const devices = try parse(input, allocator);
+    defer allocator.free(devices);
+    return countPaths1(devices, you);
+}
+
+fn countPaths1(devices: [][maxOutputs]u16, device: u16) usize {
+    var count: usize = 0;
+    for (devices[device]) |output| {
+        switch (output) {
+            0 => return count,
+            out => return 1,
+            else => count += countPaths1(devices, output),
+        }
+    }
+    unreachable;
 }
 
 pub fn part2(input: []const u8, allocator: std.mem.Allocator) !usize {
-    _ = input;
-    _ = allocator;
-    return 0;
+    const devices = try parse(input, allocator);
+    defer allocator.free(devices);
+    var memo: [maxDevices * 4 + 1]?usize = @splat(null);
+    return countPaths2(devices, svr, &memo, false, false);
+}
+
+fn countPaths2(devices: [][maxOutputs]u16, device: u16, memo: *[maxDevices * 4 + 1]?usize, passed_dac: bool, passed_fft: bool) usize {
+    const memoKey = @as(usize, device) * 4 + @as(u2, @intFromBool(passed_dac)) * 2 + @intFromBool(passed_fft);
+    if (memo[memoKey]) |cached| {
+        return cached;
+    }
+
+    var count: usize = 0;
+    for (devices[device]) |output| {
+        switch (output) {
+            0 => {
+                memo[memoKey] = count;
+                return count;
+            },
+            dac => count += countPaths2(devices, output, memo, true, passed_fft),
+            fft => count += countPaths2(devices, output, memo, passed_dac, true),
+            out => if (passed_dac and passed_fft) {
+                return 1;
+            } else {
+                return count;
+            },
+            else => count += countPaths2(devices, output, memo, passed_dac, passed_fft),
+        }
+    }
+    unreachable;
 }
 
 pub fn main() !void {
     try @import("main.zig").aocRun(@src(), @This());
 }
 
-test "example" {
+test "nameToId" {
+    try std.testing.expectEqual(@as(u16, 1), nameToId("aaa"));
+    try std.testing.expectEqual(@as(u16, 2), nameToId("aab"));
+    try std.testing.expectEqual(@as(u16, 26), nameToId("aaz"));
+    try std.testing.expectEqual(@as(u16, 26 * 26 * 26 - 26 * 26 + 1), nameToId("zaa"));
+    try std.testing.expectEqual(@as(u16, 26 * 26 * 26), nameToId("zzz"));
+}
+
+test "example1" {
     const input =
-        \\
+        \\aaa: you hhh
+        \\you: bbb ccc
+        \\bbb: ddd eee
+        \\ccc: ddd eee fff
+        \\ddd: ggg
+        \\eee: out
+        \\fff: out
+        \\ggg: out
+        \\hhh: ccc fff iii
+        \\iii: out
     ;
-    try std.testing.expectEqual(@as(usize, 0), try part1(input, std.testing.allocator));
-    try std.testing.expectEqual(@as(usize, 0), try part2(input, std.testing.allocator));
+    try std.testing.expectEqual(@as(usize, 5), try part1(input, std.testing.allocator));
+}
+
+test "example2" {
+    const input =
+        \\svr: aaa bbb
+        \\aaa: fft
+        \\fft: ccc
+        \\bbb: tty
+        \\tty: ccc
+        \\ccc: ddd eee
+        \\ddd: hub
+        \\hub: fff
+        \\eee: dac
+        \\dac: fff
+        \\fff: ggg hhh
+        \\ggg: out
+        \\hhh: out
+    ;
+    try std.testing.expectEqual(@as(usize, 2), try part2(input, std.testing.allocator));
 }
