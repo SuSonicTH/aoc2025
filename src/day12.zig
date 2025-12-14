3dd9744cd9c1ba2920a6b9743c3d8a6b95e08522
Michael Wolf <susonicth@gmail.com>
2025-12-14 20:01:29 +0100

    day 12/1

--- src/day12.zig
+++ src/day12.zig
@@ -1,10 +1,30 @@
 const std = @import("std");
 pub const stdout = @import("stdout.zig");
 
+const shapes = 6;
+const shapeSize = 16;
+
 pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    _ = input;
     _ = allocator;
-    return 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input[shapes * shapeSize ..], '\n');
+
+    var validRegions: usize = 0;
+    while (lines.next()) |line| {
+        var packages = std.mem.tokenizeScalar(u8, line[6..], ' ');
+        var packageCount: u16 = 0;
+        while (packages.next()) |package| {
+            packageCount += try std.fmt.parseInt(u16, package, 10);
+        }
+
+        const widht = try std.fmt.parseInt(u16, line[0..2], 10);
+        const height = try std.fmt.parseInt(u16, line[3..5], 10);
+        const availablePackageArea = widht / 3 * height / 3;
+        if (availablePackageArea >= packageCount) {
+            validRegions += 1;
+        }
+    }
+    return validRegions;
 }
 
 pub fn part2(input: []const u8, allocator: std.mem.Allocator) !usize {
@@ -18,9 +38,6 @@ pub fn main() !void {
 }
 
 test "example" {
-    const input =
-        \\
-    ;
-    try std.testing.expectEqual(@as(usize, 0), try part1(input, std.testing.allocator));
-    try std.testing.expectEqual(@as(usize, 0), try part2(input, std.testing.allocator));
+    const input = @embedFile("day12.txt");
+    try std.testing.expectEqual(@as(usize, 408), try part1(input, std.testing.allocator));
 }
